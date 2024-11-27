d8304311f3b3447ec955aeb3ad4f006f388cd2b4
= <=>
2024-11-27 14:56:05 +0100

    feat: add comments and autorization/authentication for comments using utterances for easy comments development

--- src/app/(blog)/(posts)/layout.tsx
+++ src/app/(blog)/(posts)/layout.tsx
@@ -1,3 +1,4 @@
+import { Comments } from '@/components/Comments'
 import type { ReactNode } from 'react'
 
 export default function PostLayout({ children }: { children: ReactNode }) {
@@ -6,6 +7,9 @@ export default function PostLayout({ children }: { children: ReactNode }) {
       <div className="prose prose-zinc md:prose-lg mx-auto max-w-[800px] md:max-w-3xl prose-h1:leading-tight prose-h1:text-4xl prose-h2:text-2xl prose-blockquote:bg-blogSecondary50 prose-blockquote:border-l-4 prose-blockquote:border-blogSecondary100 prose-blockquote:rounded-xl md:prose-blockquote:p-8 prose-blockquote:p-4 md:prose-blockquote:text-2xl prose-blockquote:italic">
         {children}
       </div>
+      <div className="h-px w-full bg-blogDarkGray" />
+      <h2 className="text-3xl font-bold text-center">Comentários</h2>
+      <Comments />
     </div>
   )
 }
