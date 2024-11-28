794cddf6089cdb158ba6a3f4a381ebb5db3b02bb
= <=>
2024-11-28 15:14:14 +0100

    feat: Show the moments when the post was created, briefly.

--- src/components/ArticlesSection/SectionPost/Poster.tsx
+++ src/components/ArticlesSection/SectionPost/Poster.tsx
@@ -9,6 +9,25 @@ type PosterProps = {
   }
 }
 
+// Função para formatar a diferença de tempo, quando tiver mais de um ano, mostrar anos, o mesmos com semanas, dias, meses e tudo mais.
+function formatTimeDifference(publishDate: Date): string {
+  const now = new Date()
+  const diffInSeconds = Math.floor(
+    (now.getTime() - publishDate.getTime()) / 1000,
+  )
+
+  if (diffInSeconds < 60) return 'Agora mesmo'
+  if (diffInSeconds < 3600) return `${Math.floor(diffInSeconds / 60)} min`
+  if (diffInSeconds < 86400) return `${Math.floor(diffInSeconds / 3600)} h`
+  if (diffInSeconds < 7 * 86400) return `${Math.floor(diffInSeconds / 86400)} d`
+  if (diffInSeconds < 30 * 86400)
+    return `${Math.floor(diffInSeconds / (7 * 86400))} S`
+  if (diffInSeconds < 365 * 86400)
+    return `${Math.floor(diffInSeconds / (30 * 86400))} M`
+
+  return `${Math.floor(diffInSeconds / (365 * 86400))} A`
+}
+
 export function Poster({ post, name, imageUrl }: PosterProps) {
   return (
     <div className="flex w-full items-center gap-4">
@@ -22,7 +41,9 @@ export function Poster({ post, name, imageUrl }: PosterProps) {
             })}
           </time>
           <span>∙</span>
-          <span>3 Min</span>
+          <span className="truncate">
+            {formatTimeDifference(new Date(post.publishDate))}
+          </span>
         </p>
       </div>
     </div>
